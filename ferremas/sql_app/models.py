00e7734ba46588ebd266591b167362e24044303d
bastianPino <bast.pino@duocuc.cl>
2024-06-18 22:55:06 -0400

    crear models
    se crean models.py

---
+++ ferremas/sql_app/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from Ferremas.sql_app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    role = Column(String)
+
+    cart_items = relationship("CartItem", back_populates="owner")
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer, ForeignKey("products.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    quantity = Column(Integer)
+
+    owner = relationship("User", back_populates="cart_items")
+    product = relationship("Product")
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Integer)
