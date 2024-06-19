241fd1792b712e6c89698fd157b0f16467b3562f
Rubione9314 <mo.rubio@duocuc.cl>
2024-06-19 01:22:23 -0400

    Orden de muestra
    orden de muestra

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(users.router, prefix="/users")
+app.include_router(inventory.router, prefix="/inventory")
+app.include_router(items.router, prefix="/items")
 app.include_router(auth.router, prefix="/auth")
 app.include_router(cart_items.router, prefix="/cart_items")
-app.include_router(inventory.router, prefix="/inventory")
+app.include_router(purchase_details.router, prefix="/purchase_details")
 app.include_router(items.router, prefix="/items")
 app.include_router(pay_service.router, prefix="/pay_service")
 app.include_router(productos.router, prefix="/productos")
-app.include_router(purchase_details.router, prefix="/purchase_details")
 app.include_router(receipts.router, prefix="/receipts")
 app.include_router(sales.router, prefix="/sales")
-app.include_router(users.router, prefix="/users")
+
