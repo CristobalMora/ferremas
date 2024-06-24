c645a60660a610617935b783ddd155ff4702238b
Rubione9314 <mo.rubio@duocuc.cl>
2024-06-24 00:53:20 -0400

    ahora si
    asdasd

--- Ferremas/app/domain/dispatch/service.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from . import repository, schemas
-
-def create_dispatch(db: Session, dispatch: schemas.DispatchCreate, user_id: int):
-    total_cost = 3000  # Coste extra fijo
-    return repository.create_dispatch(db, dispatch, user_id, total_cost)
